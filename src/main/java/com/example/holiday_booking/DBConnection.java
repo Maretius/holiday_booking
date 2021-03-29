d49ad0016986c18ba78051af105cc64e589b18da
Johann <j.k.nwm99@gmail.com>
2021-03-29 13:26:33 +0200

    Verteilte Arbeit ist doppelte Arbeit

--- src/main/java/com/example/holiday_booking/DBConnection.java
+++ src/main/java/com/example/holiday_booking/DBConnection.java
@@ -6,9 +6,8 @@ public class DBConnection {
 
     // JDBC driver name and database URL
     static final String JDBC_DRIVER = "org.mariadb.jdbc.Driver";
-    static final String DB_URL = "jdbc:mariadb://localhost:3306/holiday_booking";
-    static final String USER = "root";
-    static final String PASS = null;
+    static final String USER = "replica";
+    static final String PASS = "replica";
 
 
     public static void main(String[] args) throws SQLException {
@@ -16,6 +15,7 @@ public class DBConnection {
     }
 
     public static void connecttoDBtoUpdate(String sql) throws ClassNotFoundException, SQLException {
+        String DB_URL = "jdbc:mariadb://192.168.0.106:3306/holiday_booking";
         Connection conn = null;
         Statement stmt = null;
             Class.forName(JDBC_DRIVER);
@@ -25,6 +25,7 @@ public class DBConnection {
     }
 
     public static ResultSet connecttoDBtoSelect(String sql) throws SQLException, ClassNotFoundException {
+        String DB_URL = "jdbc:mariadb://192.168.0.104:3306/holiday_booking";
         Connection conn = null;
         Statement stmt = null;
             Class.forName(JDBC_DRIVER);
